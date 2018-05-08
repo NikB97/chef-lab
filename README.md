db27fd2af4bcc42870c0c6a9dc7692c49523f920
NikB97 <nikbuzin97@gmail.com>
2018-05-08 23:48:53 +0300

    cookbook repo added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # chef-lab
 repository for lab
+
+Here you can see the cookbook that install jboss with the sample application !
---
+++ jboss_cookbook/recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook:: jboss_cookbook
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+# java
+include_recipe 'java'
+
+# creating user
+user 'jboss' do
+  comment 'jboss user'
+  shell '/bin/bash'
+end
+
+# downloading jboss.zip
+remote_file '/home/vagrant/jboss.zip' do
+  source 'https://kent.dl.sourceforge.net/project/jboss/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip'
+  show_progress true
+end
+
+# installing unzip
+package 'unzip'
+
+# unziping archive
+bash 'unarchive' do
+  code <<-EOH
+    mkdir -p /opt/jboss/
+    unzip /home/vagrant/jboss.zip -d /opt/
+    cp -r /opt/jboss-5.1.0.GA/* /opt/jboss/
+    chown -R jboss:jboss /opt/jboss
+    EOH
+end
+
+# including jboss service
+template '/etc/systemd/system/jboss.service' do
+  source 'jboss.service.erb'
+end
+
+# deploying sample.war
+remote_file '/opt/jboss/server/default/deploy/sample.war' do
+  source 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
+  show_progress true
+end
+
+# enabling and starting jboss
+service 'jboss' do
+  action [:enable, :start]
+end
